17366f2f52331ba3886f3a131e6f99a998d1616a
moshena <moshikoabc@gmail.com>
2017-02-08 19:44:08 +0200

    API

---
+++ source/flowChart.py
@@ -0,0 +1,23 @@
+
+
+class flowChart:
+
+    objects = None
+    def __init__(self,objects):
+        self.objects = objects
+
+    def startProcess(self):# this funcation will manage the others one and return status to the caller
+        pass
+
+    def objectVerifier(self):# here we will check the objects
+        pass
+
+    def objectAnalyzer(self):#here we will anlayze the data
+        pass
+
+    def ruler(self):#here we will check the data by the flowChart rules
+        pass
+
+
+
+
