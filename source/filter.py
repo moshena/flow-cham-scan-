fa5cc3e238d06618f26cc91f59cc6566fd57a700
moshena <moshikoabc@gmail.com>
2017-02-08 19:26:48 +0200

    Update filter.py

---
+++ source/filter.py
@@ -0,0 +1,19 @@
+class classifier:
+
+
+    image = None
+    def __init__(self,image):
+        self.image = image
+
+    def startFiler(self):# this funcation will manage the others one and return status to the caller
+        pass
+
+    def validation(self):# here we will check the image format, size,acsess
+        pass
+
+    def ImageConvertor(self):#here we will filter te image
+        pass
+
+    def copyImage(self):#copy the image to get ful control
+        pass
+
