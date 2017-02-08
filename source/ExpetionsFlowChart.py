6ad07f257227111e95937c90c004e35781bd8a55
moshena <moshikoabc@gmail.com>
2017-02-08 18:46:50 +0200

    add Exaptions

---
+++ source/ExpetionsFlowChart.py
@@ -0,0 +1,92 @@
+class Error(Exception):
+    pass
+
+class InputError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expr -- input expression in which the error occurred
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
+
+class UnRecognizeShapeError(Error):
+    """Raised when an operation attempts a state transition that's not
+    allowed.
+
+    Attributes:
+        prev -- state at beginning of transition
+        next -- attempted new state
+        msg  -- explanation of why the specific transition is not allowed
+    """
+
+    def __init__(self, prev, next, msg):
+        self.prev = prev
+        self.next = next
+        self.msg = msg
+
+
+class SizeError(Error):
+    """Raised when an operation attempts a state transition that's not
+    allowed.
+
+    Attributes:
+        prev -- state at beginning of transition
+        next -- attempted new state
+        msg  -- explanation of why the specific transition is not allowed
+    """
+
+    def __init__(self, prev, next, msg):
+        self.prev = prev
+        self.next = next
+        self.msg = msg
+
+class UnsupportedformatError(Error):
+    """Raised when an operation attempts a state transition that's not
+    allowed.
+
+    Attributes:
+        prev -- state at beginning of transition
+        next -- attempted new state
+        msg  -- explanation of why the specific transition is not allowed
+    """
+
+    def __init__(self, prev, next, msg):
+        self.prev = prev
+        self.next = next
+        self.msg = msg
+
+
+class UnkownshapeError(Error):
+    """Raised when an operation attempts a state transition that's not
+    allowed.
+
+    Attributes:
+        prev -- state at beginning of transition
+        next -- attempted new state
+        msg  -- explanation of why the specific transition is not allowed
+    """
+
+    def __init__(self, prev, next, msg):
+        self.prev = prev
+        self.next = next
+        self.msg = msg
+
+class UnknownError(Error):
+    """Raised when an operation attempts a state transition that's not
+    allowed.
+
+    Attributes:
+        prev -- state at beginning of transition
+        next -- attempted new state
+        msg  -- explanation of why the specific transition is not allowed
+    """
+
+    def __init__(self, prev, next, msg):
+        self.prev = prev
+        self.next = next
+        self.msg = msg
+
