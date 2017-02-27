40e7958c8e7069c4f1467c10308939e7c3dcc8a5
Moshe <moshikoabc@gmail.com>
2017-02-27 20:30:24 +0200

    add more functionality

--- source/HEADER.py
+++ source/HEADER.py
@@ -2,9 +2,14 @@ import objects
 
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~successful HEADERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
+#~~~~~~~~~~~~~~~~~~~~~~~successful HEADERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~``<100
 unimplement = objects.HEADER(0,"Not implemented yet")
+success = objects.HEADER(1,"Successful")
 
-#~~~~~~~~~~~~~~~~~~~~~~~Error HEADERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~Error HEADERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~``>100
+invalidParam = objects.HEADER(101,"Invalid parameters")
+invalidObj = objects.HEADER(102,"unknowen object")
 
 
--- source/classifier.py
+++ source/classifier.py
@@ -2,24 +2,25 @@ import numpy as np
 import HEADER
 
 
-
 class classifier:
 
     imagelists = None
     image = None
+    cursorX = None
+    cursorY = None
+
     def __init__(self,image):
         self.image = image
+        self.cursorX = 0
+        self.cursorY = 0
 
     def startProcess(self):# this funcation will manage the others one and return status to the caller
         return HEADER.unimplement
 
-    def imageVerifier(self):# here we will check the image
-        pass
-
     def ObjectSpliter(self):#here we will get image and split here to shapes +open object to save location and more
         pass
 
-    def StartRecognize(self):#from here we will start wo recognize each shape image
+    def StartRecognize(self,subimage):#from here we will start wo recognize each shape image
         pass
 
     def Recognizer(self,image):#process engine
